96f7c76d882f1cd95175cf70c1cddb869baffa9e
Kyle <kyleferguson91@gmail.com>
2022-08-18 21:40:06 -0400

    started ui

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 
 const container = document.querySelector(".container")
 
-for (let i = 0; i<= 15; i++) {
+for (let i = 0; i< 256; i++) {
+
+    
     container.appendChild(document.createElement("div"))
+   
 }
+
+
+
+
+function hover(element) {
+    
+    element.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "pink"
+       
+
+    })
+
+    element.addEventListener("mouseout", (e) => {
+        e.target.style.backgroundColor = "white"
+       
+
+    })
+
+
+}
+
+let pete = document.querySelectorAll("div")
+
+pete.forEach((elem,ind,arr) =>
+{hover(elem)})
+
+
+const button = document.querySelector(".squares")
+
+function getSquares() {
+    let squares = prompt("how many squares would you like?", "enter a number")
+    
+}
+
+button.addEventListener("click", getSquares)
